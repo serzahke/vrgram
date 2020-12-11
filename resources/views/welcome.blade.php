a01eea3d54adf8a26c765550a9c907f79db3c5ee
arman kalhori <arman.kalhori@gmail.com>
2020-12-12 01:38:57 +0330

    1st commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,136 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        {{--1st segment --}}
+        <div class="row">
+            <div class="col-md-6">
+                <h1>طراحی گرافیک کنترل و فرماندهی رزمایشگاه</h1>
+            </div>
+            <div class="col-md-6">
+                <div class="d-flex justify-content-center">
+                    <img src="img/logo.png" alt="" class="w-75">
+                </div>
+            </div>
+        </div>
+
+        {{--2nd segment --}}
+        <div class="row">
+            <h1>تکنولوژی‌های استفاده مورد استفاده</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/adobe-xd.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Adobe Xd</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/zeplin.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Zeplin</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/php.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Php</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/laravel.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Laravel</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/firebase.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Firebase</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/javascript.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Javascript</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/vue.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Vue js</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/html-5.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Html-5</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/css-3.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Css-3</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/bootstrap-4.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Bootstrap-4</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/threejs.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Three.js</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/d3.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">D3.js</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/chart-js.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Chart js</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card mb-2">
+                    <img src="img/unity.svg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-text text-center">Unity</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
